933768cd7db8d30229e224c51e4128da77faff64
이도현 <ksmfou98@naver.com>
2022-02-07 20:53:19 +0900

    feat: BreakPoint 생성

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,12 @@
+import Header from "components/Header";
 import React, { ReactElement } from "react";
 
 function MainPage(): ReactElement {
-  return <div>MainPage</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default MainPage;
