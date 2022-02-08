9b91d1c46c3f7300247d814672c6f5c238a74fc8
이도현 <ksmfou98@naver.com>
2022-02-08 19:37:38 +0900

    feat: 필터링 박스 스타일링

--- src/components/FilterBox.tsx
+++ src/components/FilterBox.tsx
@@ -1,4 +1,6 @@
-import React, { ReactElement } from "react";
+import { ArrowDropDown24Icon, Refresh24Icon } from "assets";
+import React, { ReactElement, useState } from "react";
+import styled from "styled-components";
 
 type FilterBoxProps = {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -25,40 +27,119 @@ function FilterBox({
   };
 
   return (
-    <div>
-      <span>가공방식: {checkedLength(methods)}</span>
-      {methods.map((method) => (
-        <div key={method}>
-          <input
-            type="checkbox"
-            name={method}
-            onChange={onChange}
-            checked={isChecked(method)}
-          />
-          <label>{method}</label>
-        </div>
-      ))}
+    <FilterBoxContainer>
+      <BoxStyled>
+        <span>가공방식({checkedLength(methods)})</span>
+        <ArrowDropDownIcon />
+        <FilterMenuBox>
+          {methods.map((method) => (
+            <MenuItem key={method}>
+              <CheckBoxStyled
+                type="checkbox"
+                name={method}
+                onChange={onChange}
+                checked={isChecked(method)}
+              />
+              <CheckBoxLabel>{method}</CheckBoxLabel>
+            </MenuItem>
+          ))}
+        </FilterMenuBox>
+      </BoxStyled>
+      <BoxStyled>
+        <span>재료({checkedLength(meterials)})</span>
+        <ArrowDropDownIcon />
+        <FilterMenuBox>
+          {meterials.map((material) => (
+            <MenuItem key={material}>
+              <CheckBoxStyled
+                type="checkbox"
+                name={material}
+                onChange={onChange}
+                checked={isChecked(material)}
+              />
+              <CheckBoxLabel>{material}</CheckBoxLabel>
+            </MenuItem>
+          ))}
+        </FilterMenuBox>
+      </BoxStyled>
 
-      <br />
-
-      <span>재료: {checkedLength(meterials)}</span>
-      {meterials.map((material) => (
-        <div key={material}>
-          <input
-            type="checkbox"
-            name={material}
-            onChange={onChange}
-            checked={isChecked(material)}
-          />
-          <label htmlFor="">{material}</label>
-        </div>
-      ))}
-
-      <div>
-        <button onClick={onReset}>필터링 리셋</button>
-      </div>
-    </div>
+      <ResetButton onClick={onReset}>
+        <Refresh24Icon /> 필터링 리셋
+      </ResetButton>
+    </FilterBoxContainer>
   );
 }
+
+const FilterBoxContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const BoxStyled = styled.div`
+  height: 32px;
+  border: 1px solid ${({ theme }) => theme.colors.dateText};
+  border-radius: 4px;
+  padding: 9px 41px 9px 12px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  margin-right: 24px;
+  cursor: pointer;
+  font-size: ${({ theme }) => theme.fontSize.smallText}px;
+  &:first-child {
+    margin-right: 4px;
+  }
+  &:hover {
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+const ArrowDropDownIcon = styled(ArrowDropDown24Icon)`
+  position: absolute;
+  top: 50%;
+  right: 19px;
+  transform: translateY(-50%);
+`;
+
+const ResetButton = styled.button`
+  border: none;
+  background: none;
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: 12px;
+  font-weight: 500;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  svg {
+    margin-right: 12px;
+  }
+`;
+
+const FilterMenuBox = styled.div`
+  width: 130px;
+  position: absolute;
+  top: 36px;
+  left: 0;
+  background: ${({ theme }) => theme.colors.white};
+  z-index: 9999;
+  border: 1px solid ${({ theme }) => theme.colors.dateText};
+  padding: 17px 12px;
+  border-radius: 4px;
+`;
+
+const MenuItem = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const CheckBoxStyled = styled.input`
+  width: 18px;
+  height: 18px;
+  margin-right: 10px;
+`;
+
+const CheckBoxLabel = styled.label`
+  font-size: ${({ theme }) => theme.fontSize.text}px;
+`;
 
 export default FilterBox;
